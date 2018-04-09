0903ad37d850d27615446b0b95a9d29a129de658
chrisstpehenmiller <csmiller4567@gmail.com>
2018-04-09 12:47:20 -0500

    scoring and pointer validation

--- src/components/dice.js
+++ src/components/dice.js
@@ -12,13 +12,15 @@ class Dice extends Component {
       heldDice: [],
       bankedDice: [],
       diceToRoll: 6,
-      turnScore: 0,
+      currentScore: 0,
       totalScore: 0,
     }
   }
 
   componentDidMount = () => {
-    this.rollDice()
+    const liveDice = [];
+    for (let i = this.state.diceToRoll; i > 0; i--) { liveDice.push(this.die()) }
+    this.setState({ liveDice })
   }
 
   die = () => {
@@ -33,59 +35,66 @@ class Dice extends Component {
     heldDice = allDice.filter(d => d.status === `held`)
     const diceToRoll = liveDice.length
     this.setState({ liveDice, heldDice, diceToRoll })
-    this.turnScore(heldDice);
+    this.currentScore(heldDice);
   }
 
-  bankHeldDice = () => {
+  countPointers = heldDice => {
+    const pointers = []
+    for (let i = 1; i < 7; i++) pointers.push(heldDice.filter(d => d.value === i))
+    return pointers.map(d => d.length)
+  }
+
+  currentScore = heldDice => {
+    let currentScore = this.state.totalScore;
+    const pointers = this.countPointers(heldDice)
+    if (pointers[0] > 2) { currentScore += 250 * 2 ** (pointers[0] - 1) } else currentScore += 100 * pointers[0];
+    if (pointers[1] > 2) currentScore += 50 * 2 ** (pointers[1] - 1)
+    if (pointers[2] > 2) currentScore += 75 * 2 ** (pointers[2] - 1)
+    if (pointers[3] > 2) currentScore += 100 * 2 ** (pointers[3] - 1)
+    if (pointers[4] > 2) { currentScore += 125 * 2 ** (pointers[4] - 1) } else currentScore += 50 * pointers[4]
+    if (pointers[5] > 2) currentScore += 150 * 2 ** (pointers[5] - 1)
+    if (pointers.filter(p => p === 1).length === 6) currentScore = 1500
+    this.setState({ currentScore })
+    this.validatePointers()
+  }
+
+  bankDice = () => {
     let { heldDice, bankedDice } = this.state
     heldDice.forEach(d => d.status = `banked`)
     bankedDice = [...heldDice, ...bankedDice]
     heldDice = [];
     this.setState({ heldDice, bankedDice })
-    this.totalScore(bankedDice);
-  }
-
-  turnScore = heldDice => {
-    let turnScore = 0;
-    let ones = 100;
-    let twos = 200/3;
-    let threes = 100/3;
-    let fours = 400/3;
-    let fives = 50;
-    let sixes = 200;
-    heldDice.filter(d => d.value === 1).length === 3 && (ones = 1000/3)
-    heldDice.filter(d => d.value === 1).length === 4 && (ones = 500)
-    heldDice.filter(d => d.value === 2).length === 4 && (twos = 100)
-    heldDice.filter(d => d.value === 3).length === 4 && (threes = 150)
-    heldDice.filter(d => d.value === 4).length === 4 && (fours = 200)
-    heldDice.filter(d => d.value === 5).length === 3 && (fives = 500/3)
-    heldDice.filter(d => d.value === 5).length === 4 && (fives = 250)
-    heldDice.filter(d => d.value === 6).length === 4 && (sixes = 300)
-    heldDice.forEach(d => {
-      if (d.value === 1) turnScore += ones;
-      if (d.value === 2) turnScore += twos;
-      if (d.value === 3) turnScore += threes;
-      if (d.value === 4) turnScore += fours;
-      if (d.value === 5) turnScore += fives;
-      if (d.value === 6) turnScore += sixes;
-    })
-    if (turnScore % 50 !== 0) turnScore = 0;
-    this.setState({ turnScore })
+    this.totalScore()
   }
 
   totalScore = () => {
-    let { turnScore } = this.state
-    const totalScore = this.state.totalScore += turnScore
-    turnScore = 0;
-    this.setState({ turnScore, totalScore })
+    const totalScore = this.state.currentScore
+    this.setState({ totalScore })
   }
 
   rollDice = () => {
-    this.bankHeldDice();
-    const liveDice = [];
-    for (let i = this.state.diceToRoll; i > 0; i--) { liveDice.push(this.die()) }
-    this.setState({ liveDice })
+    if (!this.validatePointers()) { document.getElementById('pointer-error').classList.add(`visible`) }
+    else {
+      this.bankDice();
+      const liveDice = [];
+      for (let i = this.state.diceToRoll; i > 0; i--) { liveDice.push(this.die()) }
+      this.setState({ liveDice })
+    }
   }
+
+  validatePointers = () => {
+    const { heldDice } = this.state
+    if (heldDice.length === 0) return false
+    let valid = true;
+    const pointers = this.countPointers(heldDice)
+    pointers.forEach((p, i) => {
+      if (p > 0 && p < 3 && (i + 1 !== 1 && i + 1 !== 5))
+        valid = false;
+    })
+    if (valid) { document.getElementById('pointer-error').classList.remove(`visible`) }
+    return valid
+  }
+
 
   render() {
     console.log(`render-state:`, this.state)
@@ -105,12 +114,16 @@ class Dice extends Component {
             <h2>Banked Dice</h2>
             <BankedDice id="banked-dice" bankedDice={bankedDice} />
           </div>
-          <button
-            onClick={this.rollDice}
-            id="roll-btn">
-            Roll
+          <div id="roll-btn">
+            <button
+              value={this.validatePointers()}
+              onClick={this.rollDice}>
+              Roll
           </button>
+            <h3 id="pointer-error" className={`pointer-error`} value={this.validatePointers()}>Pointers are not valid</h3>
+          </div>
           <div>
+            <h1>Current score: {this.state.currentScore}</h1>
           </div>
         </div>
       </div>
