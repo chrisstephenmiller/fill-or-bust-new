18913da2c2d4a478c334aad1a36531fae108fa94
chrisstpehenmiller <csmiller4567@gmail.com>
2018-04-12 19:04:44 -0500

    fixed stop

--- src/components/game.js
+++ src/components/game.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Dice from './dice'
 import Roll from './roll'
-// import Stop from './stop'
+import Stop from './stop'
 import Score from './score'
 
 class Game extends Component {
@@ -11,6 +11,7 @@ class Game extends Component {
       <div id="game">
         <Dice />
         <Roll />
+        <Stop />
         <Score />
       </div>
     );
